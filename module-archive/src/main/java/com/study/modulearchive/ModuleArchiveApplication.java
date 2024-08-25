f30adaea58a64733b62eecf5164665733f892e2b
ksh <tjdgus2319@gmail.com>
2024-08-25 10:57:54 +0900

    feat: init

---
+++ module-archive/src/main/java/com/study/modulearchive/ModuleArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.study.modulearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleArchiveApplication.class, args);
+    }
+
+}
---
+++ module-archive/src/test/java/com/study/modulearchive/ModuleArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.modulearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
